4d3b6e77c18b50ee4cdcf19a1d8e5bb301a644bc
danieljanssen@canda.com <rt57@DEN0251.canda.biz>
2017-10-11 15:43:39 +0200

    remove todo

--- examples/example-ejb/src/main/java/de/th_koeln/example/shoppingcard/calculator/ShoppingCardItemCalculator.java
+++ examples/example-ejb/src/main/java/de/th_koeln/example/shoppingcard/calculator/ShoppingCardItemCalculator.java
@@ -3,7 +3,6 @@ package de.th_koeln.example.shoppingcard.calculator;
 import de.th_koeln.example.shoppingcard.entity.ShoppingCardItem;
 import de.th_koeln.example.shoppingcard.vo.TotalPrice;
 
-// TODO rt57, 11.10.2017: tests!
 public interface ShoppingCardItemCalculator {
 
 	public TotalPrice calculate(ShoppingCardItem aShoppingCardItem);
