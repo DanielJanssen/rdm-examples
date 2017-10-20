2f90b918f6d131f6ff22a8ac15deb0ba5a417e64
danieljanssen@canda.com <rt57@DEN0251.canda.biz>
2017-10-20 15:34:10 +0200

    create repository and add lib to classpath

---
+++ examples/example-ejb/src/main/java/de/th_koeln/example/shoppingcart/repository/ShoppingCartRepository.java
@@ -0,0 +1,24 @@
+package de.th_koeln.example.shoppingcart.repository;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.mysema.query.jpa.impl.JPAQuery;
+
+import de.th_koeln.example.shoppingcart.entity.QShoppingCart;
+import de.th_koeln.example.shoppingcart.entity.ShoppingCart;
+
+@Stateless
+public class ShoppingCartRepository {
+	private static final QShoppingCart SHOPPING_CART = QShoppingCart.shoppingCart;
+
+	@PersistenceContext(unitName = "examples-ejb")
+	private EntityManager em;
+
+	public List<ShoppingCart> getAllShoppingCarts() {
+		return new JPAQuery(em).from(SHOPPING_CART).list(SHOPPING_CART);
+	}
+}
