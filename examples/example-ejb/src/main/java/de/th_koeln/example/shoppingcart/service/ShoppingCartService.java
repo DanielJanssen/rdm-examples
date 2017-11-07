78be08248c350f352f55819b99ae0d7f025b8111
danieljanssen@canda.com <rt57@DEN0251.canda.biz>
2017-11-07 17:05:44 +0100

    refactoring: introduce method for add item to shoppingcard

--- examples/example-ejb/src/main/java/de/th_koeln/example/shoppingcart/service/ShoppingCartService.java
+++ examples/example-ejb/src/main/java/de/th_koeln/example/shoppingcart/service/ShoppingCartService.java
@@ -1,19 +1,27 @@
 package de.th_koeln.example.shoppingcart.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import de.th_koeln.example.shoppingcart.attribute.Currency;
 import de.th_koeln.example.shoppingcart.attribute.ShoppingCartId;
+import de.th_koeln.example.shoppingcart.entity.Article;
 import de.th_koeln.example.shoppingcart.entity.ShoppingCart;
+import de.th_koeln.example.shoppingcart.entity.ShoppingCartItem;
 import de.th_koeln.example.shoppingcart.repository.ShoppingCartRepository;
+import de.th_koeln.example.shoppingcart.vo.PricePerPiece;
 
 @Stateless
 public class ShoppingCartService {
 
 	@Inject
 	ShoppingCartRepository shoppingCartRepository;
+
+	@Inject
+	ArticleService articleService;
 
 	public List<ShoppingCart> getAllShoppingCarts() {
 		return shoppingCartRepository.getAllShoppingCarts();
@@ -25,5 +33,17 @@ public class ShoppingCartService {
 
 	public ShoppingCart saveShoppingCart(ShoppingCart aShoppingCart) {
 		return shoppingCartRepository.save(aShoppingCart);
+	}
+
+	public void addItemToShoppingCart(ShoppingCartId aShoppingCartId, ShoppingCartItem.Builder shoppingCartItem, Article anArticle) {
+		//	TODO	das setzen hier ist auch nicht optimal
+		shoppingCartItem.withArticle(anArticle);
+		//// TODO rt57, 07.11.2017: in den artikel!
+		shoppingCartItem.forPricePerPiece(new PricePerPiece.Builder().forCurrency(Currency.fromValue("EURO")).withAmount(BigDecimal.ONE).build());
+
+		ShoppingCart shoppingCart = getShoppingCart(aShoppingCartId);
+		shoppingCart.addItem(shoppingCartItem.build());
+
+		saveShoppingCart(shoppingCart);
 	}
 }
--- examples/examples-web/src/main/java/de/th_koeln/example/shoppingcard/bean/additem/ShoppingCartAddItemDialogBean.java
+++ examples/examples-web/src/main/java/de/th_koeln/example/shoppingcard/bean/additem/ShoppingCartAddItemDialogBean.java
@@ -1,7 +1,6 @@
 package de.th_koeln.example.shoppingcard.bean.additem;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
@@ -14,13 +13,11 @@ import org.primefaces.context.RequestContext;
 
 import de.th_koeln.example.event.ActionEvent;
 import de.th_koeln.example.shoppingcart.attribute.ArticleId;
-import de.th_koeln.example.shoppingcart.attribute.Currency;
+import de.th_koeln.example.shoppingcart.attribute.ShoppingCartId;
 import de.th_koeln.example.shoppingcart.entity.Article;
-import de.th_koeln.example.shoppingcart.entity.ShoppingCart;
 import de.th_koeln.example.shoppingcart.entity.ShoppingCartItem;
 import de.th_koeln.example.shoppingcart.service.ArticleService;
 import de.th_koeln.example.shoppingcart.service.ShoppingCartService;
-import de.th_koeln.example.shoppingcart.vo.PricePerPiece;
 
 @Named
 @SessionScoped
@@ -32,7 +29,7 @@ public class ShoppingCartAddItemDialogBean implements Serializable {
 	private ShoppingCartItem.Builder shoppingCartItem;
 	private List<Article> articles;
 	private String selectedArticle;
-	private ShoppingCart shoppingCart;
+	private ShoppingCartId shoppingCart;
 
 	@Inject
 	ArticleService articleService;
@@ -44,7 +41,7 @@ public class ShoppingCartAddItemDialogBean implements Serializable {
 	Event<ActionEvent> event;
 
 	public void init(@Observes AddItemEvent anEvent) {
-		shoppingCart = shoppingCartService.getShoppingCart(anEvent.getShoppingCartId());
+		shoppingCart = anEvent.getShoppingCartId();
 		shoppingCartItem = new ShoppingCartItem.Builder();
 		articles = articleService.getAllArticles();
 		RequestContext.getCurrentInstance().execute("PF('" + DIALOG + "').show()");
@@ -67,16 +64,7 @@ public class ShoppingCartAddItemDialogBean implements Serializable {
 	}
 
 	public void save() {
-		// TODO rt57, 07.11.2017: converter und direkt auf dem item arbeiten? aber schöner ist es bei ff:, dann diesen mechanismus mal verstehen...
-		//		auch das ganze zusammenbauen vll in den service?
-		shoppingCartItem.withArticle(articleService.getArticle(ArticleId.fromValue(selectedArticle)));
-
-		//// TODO rt57, 07.11.2017: in den artikel!
-		shoppingCartItem.forPricePerPiece(new PricePerPiece.Builder().forCurrency(Currency.fromValue("EURO")).withAmount(BigDecimal.ONE).build());
-
-		shoppingCart.addItem(shoppingCartItem.build());
-
-		shoppingCart = shoppingCartService.saveShoppingCart(shoppingCart);
+		shoppingCartService.addItemToShoppingCart(shoppingCart, shoppingCartItem, articleService.getArticle(ArticleId.fromValue(selectedArticle)));
 		event.fire(new FinishAddItemEvent());
 		RequestContext.getCurrentInstance().execute("PF('" + DIALOG + "').hide()");
 	}
